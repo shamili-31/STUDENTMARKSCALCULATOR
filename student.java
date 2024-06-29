6b4b3012eae13c86f8f73f56d4958facc65c7f84
Gajjala Shamili <130270992+shamili-31@users.noreply.github.com>
2024-06-29 18:53:41 +0530

    Add files via upload

---
+++ student.java
@@ -0,0 +1,58 @@
+import java.util.Scanner;
+
+public class student {
+    public static void main(String[] args) {
+        try (Scanner sc = new Scanner(System.in)) {
+            System.out.println("Enter total number of subjects: ");
+            int numSubjects = sc.nextInt();
+
+            int totalMarks = 0;
+
+            for (int i = 1; i <= numSubjects; i++) {
+                System.out.println("Enter marks for subject " + i + "(out of 100): ");
+                int marks = sc.nextInt();
+                while (marks < 0 || marks > 100) {
+                    System.out.println("Invalid marks. Please enter marks between 0 and 100.");
+                    System.out.println("Enter marks for subject " + i + "out of 100): ");
+                    marks = sc.nextInt();
+                }
+                totalMarks += marks;
+            }
+            System.out.println("Student Result: ");
+            System.out.println("Total marks obtained in all subjects: " + totalMarks);
+            int AveragePercentage = totalMarks / numSubjects;
+            System.out.println("Average Percentage: " + AveragePercentage);
+            if (AveragePercentage >= 90) {
+                System.out.println("Your grade: A+");
+            }
+
+            else if (AveragePercentage >= 80) {
+                System.out.println("Your grade: B+");
+            }
+
+            else if (AveragePercentage >= 70) {
+                System.out.println("Your grade: B");
+            }
+
+            else if (AveragePercentage >= 60) {
+                System.out.println("Your grade: C+");
+            }
+
+            else if (AveragePercentage >= 50) {
+                System.out.println("Your grade: C");
+            }
+
+            else if (AveragePercentage >= 40) {
+                System.out.println("Your grade: D+");
+            }
+
+            else if (AveragePercentage >= 30) {
+                System.out.println("Your grade: D");
+            }
+
+            else {
+                System.out.println("Your grade is: E");
+            }
+        }
+    }
+}
